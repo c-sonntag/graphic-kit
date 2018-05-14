fab817c34eefedeed72c60bd8ceb8edba1c181e3
Christophe Sonntag <15939808+c-sonntag@users.noreply.github.com>
2018-05-14 04:26:35 +0200

    [Update] debug stream tools

--- include/graphic_toolkit/debug_stream.h
+++ include/graphic_toolkit/debug_stream.h
@@ -6,25 +6,29 @@
 #include <QPoint>
 
 #include <iostream>
+#include <iomanip>
 
 #define OUTPUT_VAR(var) " " #var "(" << var << ") "
 #define OV(var) OUTPUT_VAR(var)
 
 inline std::ostream & operator<<( std::ostream & os, const QVector2D & v2 )
 {
-  os << v2.x() << "," << v2.y();
+  os << std::setprecision( 2 );
+  os << std::setw( 5 ) << v2.x() << "," << std::setw( 5 ) << v2.y();
   return os;
 }
 
 inline std::ostream & operator<<( std::ostream & os, const QVector3D & v3 )
 {
-  os << v3.x() << "," << v3.y() << "," << v3.z();
+  os << std::setprecision( 2 );
+  os << std::setw( 5 ) << v3.x() << "," << std::setw( 5 ) << v3.y() << "," << std::setw( 5 ) << v3.z();
   return os;
 }
 
 inline std::ostream & operator<<( std::ostream & os, const QPoint & p )
 {
-    os << p.x() << "," << p.y();
-    return os;
+  os << std::setprecision( 2 );
+  os << std::setw( 5 ) << p.x() << "," << std::setw( 5 ) << p.y();
+  return os;
 }
 
