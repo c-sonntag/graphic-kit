9992e7ee905b6f5d403025cb2d16be9a94b9a731
Christophe Sonntag <15939808+c-sonntag@users.noreply.github.com>
2018-05-15 14:52:10 +0200

    [Versioning]

---
+++ include/graphic_toolkit/constants.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <cmath>
+
+namespace graphic_toolkit {
+  namespace constants {
+
+    static constexpr float pi = float( M_PI );
+    static constexpr float pi_tour = float( 2.0 * M_PI );
+    static constexpr float demi_tour = float( M_PI_2 );
+    static constexpr float quart_tour = float( M_PI_4 );
+
+  }
+}
